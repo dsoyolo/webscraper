b3e7aaebec231ccfbd08a67560e4f7743684e2a2
ubimars <marcelpenton+github@gmail.com>
2021-02-17 18:07:01 -0500

    Initial code commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # webscraper
 
 I wanted to build my very own webscraper in Python using the Beautiful Soup library.
+
++ Note that some bot detections such as Imperva will detect certain scraping frameworks.
--- webscraper.py
+++ webscraper.py
@@ -1,2 +1,8 @@
-msg = "Hello World"
-print(msg)
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://cbnorthbay.com/real-estate-properties/?addressmls=&status=For+sale&minprice=&maxprice=&minbed=&minbath=&property_type=&townvillages=North+Bay&structure_type=&waterfront=&listingssearch_submit=Search&df=1'
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content, "html.parser")
+
+print(content)
